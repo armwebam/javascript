cd45647efb92f3ab72b1acfd08c8df8a52fc2a4b
karakala14 <zkhacatryan@gmail.com>
2017-06-19 15:07:56 +0500

    newcommit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+function moveleft()
+{
+document.getElementById('image').style.position="absolute";
+document.getElementById('image').style.left="0";
+}
+function moveback()
+{
+document.getElementById('image').style.position="absolute";
+document.getElementById('image').style.right="0";
+}
